9eefb7fba368837b1a7333d4372d589be31240d0
sirsuccess <kanuamani@gmail.comgit config --global user.email kanuamani@gmail.comgit config --global user.email kanuamani@gmail.com>
2019-11-04 22:04:24 +0100

    all function complete

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import SearchComponent from "./component/search";
+
+import "./App.css";
+
+function App() {
+  const [movie, setMovie] = useState({});
+
+  useEffect(() => {
+    const fetchContact = async () => {
+      const movieFetch = await axios.get(
+        "http://www.omdbapi.com/?i=tt3896198&apikey=53b5bde7"
+      );
+      setMovie(movieFetch.data);
+    };
+    fetchContact();
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="movieCard">
+        <div className="title">{movie.Title}</div>
+        <div className="imgDiv">
+          <img src={movie.Poster} alt="movie poster" />
+        </div>
+        <div className="movieBotom">
+          <h1>{movie.Title}</h1>
+          <div className="movieDetails">
+            <div className="release">
+              <div className="movieDetailsTitle">Released:</div>
+              {movie.Released}
+            </div>
+            <div className="release">
+              <div className="movieDetailsTitle">Rated:</div>
+              {movie.imdbRating}
+            </div>
+            <div className="release">
+              <div className="movieDetailsTitle">Rated:</div>
+              {movie.Rated}
+            </div>
+            <div className="release">
+              <div className="movieDetailsTitle">Runtime:</div>
+              {movie.Runtime}
+            </div>
+          </div>
+          <p>{movie.Plot}</p>
+        </div>
+      </div>
+      <SearchComponent parentState={{ movie, setMovie }} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/search.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function SearchComponent({ parentState }) {
+  const [movieSearch, setMovieSearch] = useState([]);
+  const { movie, setMovie } = parentState;
+
+  async function searchFunct(e) {
+    let typedItem = e.target.value;
+    const movieFetch = await axios.get(
+      `http://www.omdbapi.com/?apikey=53b5bde7&s=${typedItem}`
+    );
+
+    if (!movieFetch.data.Error) {
+      setMovieSearch(movieFetch.data.Search);
+    } else {
+      setMovieSearch([]);
+    }
+  }
+
+  async function getDetails(movieID) {
+    const movieFetch = await axios.get(
+      `http://www.omdbapi.com/?i=${movieID}&apikey=53b5bde7`
+    );
+    setMovie(movieFetch.data);
+  }
+  return (
+    <div className="search">
+      <div className="innerSearch">
+        <input
+          type="text"
+          name="item"
+          placeholder="Search movie..."
+          onChange={searchFunct}
+        ></input>
+        <button className="btn">X</button>
+      </div>
+      {movieSearch.map(movie => {
+        return (
+          <li onClick={() => getDetails(movie.imdbID)}>
+            <div className="searchImg">
+              <img src={movie.Poster} alt="movie poster" />
+            </div>
+            {movie.Title}
+          </li>
+        );
+      })}
+    </div>
+  );
+}
